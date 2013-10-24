0f7df8a9e9378c2adc9a733f4c63452c69b2fceb
Tim Hall <tim.hall.engr@gmail.com>
2013-10-24 13:19:06 -0400

    Add initial test server

---
+++ specs/server.js
@@ -0,0 +1,56 @@
+var express = require('express');
+var app = express();
+
+app.use(plain());
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(function(req, res, next){
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+
+// Standard
+app.get('/get', standardResponse);
+app.post('/post', standardResponse);
+app.post('/text', standardResponse);
+app.put('/put', standardResponse);
+app.patch('/patch', standardResponse);
+app.delete('/delete', standardResponse);
+
+// Statuses
+app.get('/status/:code', function(req, res) {
+  res.send(parseInt(req.params.code));
+});
+
+// Timeout
+app.get('/timeout', function(req, res) {
+  var ms = req.query.ms || 10000;
+  setTimeout(function() {
+    res.send(200, 'Finally resolves (took ' + ms/1000 + ' seconds)');
+  }, ms);
+});
+
+function standardResponse(req, res) {
+  res.send(200, {
+    method: req.route.method.toUpperCase(),
+    query: req.query,
+    body: req.text || req.body
+  });
+}
+
+function plain() {
+  return function(req, res, next){
+    if (req.is('text/*')) {
+      req.text = '';
+      req.setEncoding('utf8');
+      req.on('data', function(chunk) { req.text += chunk });
+      req.on('end', next);
+    } else {
+      next();
+    }
+  }
+}
+
+app.listen(3000);
+console.log('Listening on port 3000');
